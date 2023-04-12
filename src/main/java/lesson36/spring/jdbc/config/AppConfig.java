d5f54e4bc81985ced2b0d8f11d2a7bcd0b5e52b8
SPasko <SPasko@IEVR10024.on.seo>
2023-04-12 21:29:37 +0300

    fix tips

--- src/main/java/lesson36/spring/jdbc/config/AppConfig.java
+++ src/main/java/lesson36/spring/jdbc/config/AppConfig.java
@@ -7,8 +7,6 @@ import javax.sql.DataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -59,10 +57,5 @@ public class AppConfig {
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
         return new JdbcTemplate(dataSource);
-    }
-
-    @Bean
-    public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager();
     }
 }
